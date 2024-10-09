2801f4f1370860654da2e6339c3de5f6e5482b56
Jim Brown <jpb@jimby.name>
2024-10-09 10:39:19 -0400

    Add files via upload

---
+++ 14.0/f14_0.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#
+# 14.0
+#
+# -device and -blockev from qemu(1)
+#
+#  The most explicit way to describe disks is to use a combination of
+#  -device to specify the hardware device and -blockdev to describe the
+#  backend. The device defines what the guest sees and the backend
+#  describes how QEMU handles the data. It is the only guaranteed stable
+#  interface for describing block devices and as such is recommended for
+#  management tools and scripting.
+#
+
+
+/usr/local/bin/qemu-system-x86_64  -monitor stdio \
+  -cpu qemu64 \
+  -vga std \
+  -m 4096 \
+  -smp 4   \
+  -cdrom ../../ISO/fbsd14_0.iso \
+  -boot order=cd,menu=on \
+  -blockdev driver=file,node-name=myfile,filename=./fbsd14_0.qcow2 \
+  -blockdev driver=qcow2,node-name=myqcow2,file=myfile,cache-size=16777216 \
+  -device virtio-blk-pci,drive=myqcow2,bootindex=1  \
+  -netdev tap,id=nd0,ifname=tap0,script=no,downscript=no,br=bridge0 \
+  -device virtio-net-pci,netdev=nd0,mac=52:54:6c:65:14:00 \
+  -name \"fbsd14.0\"
+
+
