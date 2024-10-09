8a5615b70daa809a069de1ee73195fb297433418
Jim B. <jpb@jimby.name>
2024-10-09 17:16:55 -0400

    Adjusted filenames and content for correct version numbers.

--- 09.0/f09_0.sh
+++ 09.0/f09_0.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 #
-# f13.0.sh
+# f09.0.sh
 #
 /usr/local/bin/qemu-system-x86_64  -monitor stdio \
   -cpu qemu64 \
   -vga std \
   -m 4096 \
   -smp 4 \
-  -drive file=./fbsd13_0.iso,index=2,media=cdrom \
+  -drive file=./fbsd09_0.iso,index=2,media=cdrom \
   -boot order=cda,menu=on \
-  -blockdev driver=file,filename=./fbsd13_0.qcow2,node-name=myfile \
+  -blockdev driver=file,filename=./fbsd09_0.qcow2,node-name=myfile \
   -blockdev driver=qcow2,file=myfile,node-name=myqcow2,cache-size=16777216 \
   -device virtio-blk-pci,drive=myqcow2,bootindex=1  \
   -netdev tap,id=nd0,ifname=tap0,script=no,downscript=no,br=bridge0 \
-  -device virtio-net-pci,netdev=nd0,mac=56:00:00:13:00:00 \
-  -name \"fbsd13.0\"
+  -device virtio-net-pci,netdev=nd0,mac=56:00:00:09:00:00 \
+  -name \"fbsd09.0\"
 
 # -runas user \           <--- for QEMU version up through 9.0
 # -run-with user=name \   <--- for QEMU version 9.1 and up
@@ -31,13 +31,13 @@
 #
 # To use:
 # 1. Create a link to the downloaded ISO using the fbsdnn_n.iso parameter above.
-#    ln -s <filepath>/FreeBSD-13.0-RELEASE-<arch>-<bootfile>.iso  fbsd13_0.iso
+#    ln -s <filepath>/FreeBSD-09.0-RELEASE-<arch>-<bootfile>.iso  fbsd09_0.iso
 # 2. Create a 2G hard disk image with qemu-img(1).
-#    qemu-image create -f qcow2 -o preallocation=full fbsd13_0.qcow2 2G
+#    qemu-image create -f qcow2 -o preallocation=full fbsd09_0.qcow2 2G
 # 3. Ensure the tap and bridge devices are set up.
 #    sudo /bin/sh mkbr.sh reset bridge0 tap0 em0  # em0 or other host interface
 # 4. Run script under sudo(8).
-#    sudo /bin/sh f13_0.sh
+#    sudo /bin/sh f09_0.sh
 # 5. Finish installation.  If the tap and bridge connections are set up
 #    correctly, and routing is correct on the host, the guest VM will be
 #    able to use the Internet immediately.
